bc540cd5c1232d68bc5d1eddecc9865bc0f0fd58
Anthony Lee <anthony.mi.lee@outlook.com>
2018-06-21 10:03:35 -0500

    Added some examples of working with ranges

--- src/main/kotlin/AppDriver.kt
+++ src/main/kotlin/AppDriver.kt
@@ -1,10 +1,21 @@
 import Color.*
+import java.util.*
 
 class Greeter(val name: String = "World") {
     fun greet() {
         println("Hello, $name!")
     }
 }
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun isLetter(c: Char) = c in  'a'..'z' || c in 'A'..'Z'
+fun isNotDigit(c: Char) = c in '0'..'9'
 
 fun main(args: Array<String>) {
     Greeter().greet()
@@ -14,6 +25,24 @@ fun main(args: Array<String>) {
     println(getWarmth(Color.ORANGE))
     println(mixOptimized(BLUE, YELLOW))
     println(mix(BLUE,YELLOW))
+
+    for (i in 1..100) {
+        print(fizzBuzz(i))
+    }
+
+    val binaryReps = TreeMap<Char, String>()
+
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryReps[c] = binary
+    }
+
+    for((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
+
+    println(isLetter('q'))
+    println(isNotDigit('x'))
 }
 
 fun getRainbowMnemonic(color: Color) =
